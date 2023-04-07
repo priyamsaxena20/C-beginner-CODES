e7bc0e6ac4f463b76a295821c0d8b20e1df2c3f3
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-04-07 18:15:16 +0530

    Create LeapYEAR.cpp

---
+++ LeapYEAR.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+  int year;
+  cin>>year;
+  cout<<"the yaer enter is"<<year;
+  if(year%400==0 || year%100!=0&& year%4==0){
+      cout<<"ye to leap yaer haui bhai";
+  }
+  else{
+      cout<<"ye nhi hai"/n;
+      
+  }
+
+    return 0;
+}
