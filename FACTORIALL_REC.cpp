48cac9e17b43e8b81461c7df62269d55ecd7e1f3
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-06-06 14:56:04 +0530

    Create FACTORIALL_REC.cpp

---
+++ FACTORIALL_REC.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+// back tracking 
+
+using namespace std;
+int print(int n){
+if(n==0)return 1;
+
+
+return n*print(n-1);
+	
+
+}
+int main(){
+	int n;
+	cout<<"enter the no you want sum to be get printed"<<endl;
+	
+	cin>>n;
+	cout<<print(n);
+	
+}
